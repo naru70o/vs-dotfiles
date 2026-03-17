c39ca1c0ee3ee30ea4721323397f742ccb6681ee
kadar_bache <khadarpaashe123@gmail.com>
2026-03-18 01:45:25 +0300

    update settings

---
+++ vscodesync.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+import platform
+import shutil
+import subprocess
+
+system = platform.system()
+home = Path.home()
+files_to_sync = ["settings.json","snippets"]
+user_choice = input("Do you want to 'backup' or 'restore' ?")
+print(home)
+current_folder = Path.cwd()
+
+if system == "Linux":
+    config_dir = home / ".config/Code/User"
+    
+    # 1. Check if the source folder exists FIRST
+    if config_dir.exists():
+        
+        # 2. Now start sync 
+        for filename in files_to_sync:
+            if user_choice == "restore":
+                source = current_folder / filename
+                destination = config_dir / filename
+            elif user_choice == "backup":
+                source = config_dir / filename 
+                destination = current_folder / filename
+            else:
+                print("Invalid choice!")
+                break
+        if source.exists():
+            if source.is_dir():
+                shutil.copytree(source, destination, dirs_exist_ok=True)
+            else:
+                shutil.copy2(source, destination)
+                print(f"Successfully {user_choice}ed {filename}")
+    else:
+        print("Error: VS Code config directory not found!, Please install vscode if it's not already installed")
+subprocess.run(["git", "init"], cwd=current_folder)
+subprocess.run(["git", "add", "."],cwd=current_folder)
+subprocess.run(["git", "commit", "-m", "update settings"], cwd=current_folder, check=True)
+
+
