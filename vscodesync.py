319898ae15c1f799a0ad2b13fda2a66c61126422
kadar_bache <khadarpaashe123@gmail.com>
2026-03-18 02:12:21 +0300

    added a commit message input

--- vscodesync.py
+++ vscodesync.py
@@ -9,6 +9,7 @@ files_to_sync = ["settings.json","snippets"]
 user_choice = input("Do you want to 'backup' or 'restore' ?")
 print(home)
 current_folder = Path.cwd()
+commit_message=input("Please put your commit message here: ")
 
 if system == "Linux":
     config_dir = home / ".config/Code/User"
@@ -36,9 +37,6 @@ if system == "Linux":
     else: print("Error: VS Code config directory not found!, Please install vscode if it's not already installed")
 print(input)
 status = subprocess.run(["git", "status"], cwd=current_folder)
-subprocess.run(["git", "init"], cwd=current_folder)
 subprocess.run(["git", "add", "."],cwd=current_folder)
-subprocess.run(["git", "commit", "-m", "update settings"], cwd=current_folder, check=True)
-subprocess.run(["git", "remote", "add", "origin", "https://github.com/naru70o/vs-dotfiles.git"], cwd=current_folder)
+subprocess.run(["git", "commit", "-m", f"{commit_message}"], cwd=current_folder, check=True)
 subprocess.run(["git", "push", "origin", "master"], cwd=current_folder)
-
